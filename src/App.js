53b93f1fc173e62a4ea24264f4f837ab27f54bdf
Abhishek Pawar <abhishekpwr944@gmail.com>
2022-12-05 20:36:59 +0530

    my commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import './mySass.scss';
 import './App.css';
 
 function App() {
+
+  const [color, setColor] = useState("red");
+  const [data,setData] = useState();
+
+  useEffect (()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((res)=> res.json())
+    .then((data)=>setData(data));
+  },[]);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Favorite color is {color} !</h1>
+
+      <button className="btn btn-primary m-1" type='button' onClick={()=>setColor("blue")}>Blue</button>
+
+      <button className="btn btn-danger m-1" type='button' onClick={()=>setColor("red")}>Red</button>
+
+      <button className="btn btn-warning m-1" type='button' onClick={()=>setColor("Yellow")}>Yellow</button>
+
+      {data && data.map((item)=>{
+        return <p key={item.id}>{item.title}</p>
+      })}
     </div>
   );
 }
